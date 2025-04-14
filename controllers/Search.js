bc699f7a8fabe1205288e6039bb18a037beedae8
D-speedster <mrspeed717@gmail.com>
2025-04-14 16:24:27 +0330

    added Search Controller

---
+++ controllers/Search.js
@@ -0,0 +1,15 @@
+const axios = require('axios');
+exports.SearchHandler = async (req, res) => {
+    // res.send(`<h1>The word Search IS : </h1> ${req.query.product}`);
+    const response = await axios.get('https://fakestoreapi.com/products');
+    let product = response.data;
+    const filteredProducts = product.filter(product => {
+        return product.id == req.query.product;
+    });
+    let TEST = filteredProducts.map(product => {
+        // res.send(`<h1>${product.title}</h1> <h2>${product.id}</h2>`)
+        res.render('Search');
+    });
+
+
+}
--- routes/search.js
+++ routes/search.js
@@ -1,18 +1,7 @@
 let express = require("express");
+
 let App = express.Router();
 const axios = require('axios');
-App.get('/', async (req, res) => {
-    // res.send(`<h1>The word Search IS : </h1> ${req.query.product}`);
-    const response = await axios.get('https://fakestoreapi.com/products');
-    let product = response.data;
-    const filteredProducts = product.filter(product => {
-        return product.id == req.query.product;
-    });
-    let TEST = filteredProducts.map(product => {
-        // res.send(`<h1>${product.title}</h1> <h2>${product.id}</h2>`)
-        res.render('Search');
-    });
-
-
-})
+const { SearchHandler } = require("../controllers/Search");
+App.get('/', SearchHandler)
 module.exports = App;
